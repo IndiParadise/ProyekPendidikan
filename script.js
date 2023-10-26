916b560c4cbd001d6ed94ea29bc57b2e93f4468d
IndiParadise <85815067+IndiParadise@users.noreply.github.com>
2023-10-26 21:35:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*function membaca() {
+    const images=["image/A.png", "image/B.png","image/C.png","image/D.png","image/E.png","image/F.png","image/G.png","image/H.png","image/I.png","image/J.png","image/K.png","image/L.png","image/M.png","image/N.png","image/O.png","image/P.png","image/Q.png","image/R.png","image/S.png","image/T.png","image/U.png","image/V.png","image/W.png","image/X.png","image/Y.png","image/Z.png"];
+    let indexOfImg=1;
+    const nextButton = document.getElementById("right");
+    const prevButton = document.getElementById("left");
+    const image = document.getElementById("letter");
+    function changeImage(){
+        image.src =images[indexOfImg];
+    }
+    function next(){
+        indexOfImg=indexOfImg+1;
+        changeImage()
+    }
+    function back(){
+        indexOfImg=indexOfImg-1;
+        changeImage()
+    }
+    };*/
